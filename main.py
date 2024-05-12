8d351764b36b2185d8f325056c05732865500a7f
YuwenTianhao <yuwentianhao@163.com>
2024-05-12 16:05:12 +0800

    实验2

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from Lab1_lib.Lab1 import lab1
+from Lab2_lib.Lab2 import lab2
 # 运行实验1
-lab1()
+# lab1()
 # lab1(img_adress='imgs/cameraman.jpg')
+# 运行实验2
+# lab2()
+# lab2(img_adress='imgs/cameraman.jpg')
